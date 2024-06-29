46eb59f9bd3debcfc7ebc92a221bb8fdc285c794
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2024-06-29 23:40:00 +0200

    don't set C locale cause imgui input float doesnt like it

--- src/main.cpp
+++ src/main.cpp
@@ -213,7 +213,7 @@ BOOL APIENTRY DllMain(HMODULE hmod, DWORD reason, PVOID)
 			    //Sleep(5000);
 
 			    // https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/setlocale-wsetlocale?view=msvc-170#utf-8-support
-			    setlocale(LC_ALL, ".utf8");
+			    //setlocale(LC_ALL, ".utf8");
 			    // This also change things like stringstream outputs and add comma to numbers and things like that, we don't want that, so just set locale on the C apis instead.
 			    //std::locale::global(std::locale(".utf8"));
 
